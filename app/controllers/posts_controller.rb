b0fcb8b44f71e503716c41e3063458388f7947f8
divtaro <katodai.pg@gmail.com>
2022-09-12 19:20:02 +0900

    createアクション設定、各アクションのインスタンス変数設定、params設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.order('id DESC')
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :shooting_month_id, :shooting_week, :impression_point, :parking_presence_id, :parking_fee_id, 
+                                 :nearest_station, :walking_time_id, :shooting_location, :other_information, :image).merge(user_id: current_user.id)
   end
 end
