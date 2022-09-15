9d757e6c7a15f89aab335ce441fdba9ff12a0959
divtaro <katodai.pg@gmail.com>
2022-09-15 13:20:48 +0900

    サーバーからクライアント側へのデータ送信設定（broadcast）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
-    redirect_to post_path(params[:post_id])
+    # Comment.create(comment_params)             //ActionCable導入により削除
+    # redirect_to post_path(params[:post_id])    //ActionCable導入により削除
+
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user}
   end
 
   private
