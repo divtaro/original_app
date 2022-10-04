da15686dbd581c0d222ef804391ddfc06f6efb0d
divtaro <katodai.pg@gmail.com>
2022-10-04 15:28:35 +0900

    users_controllerに「あるユーザーがいいねした投稿のidと投稿情報」を定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # @posts = @user.posts
     @posts = Post.order('id DESC')  
+
+    likes= Like.where(user_id: @user.id).pluck(:post_id)
+    @like_posts = Post.find(likes)
   end
 end
